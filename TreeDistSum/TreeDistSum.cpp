ec950bae3638c94eb9a4520e0fb08166199fc06a
Viktor Kustov <kustovvictor@gmail.com>
2022-10-08 01:16:33 +0300

    Memory usage optimized

--- TreeDistSum/TreeDistSum.cpp
+++ TreeDistSum/TreeDistSum.cpp
@@ -1,68 +1,59 @@
 ﻿#include<iostream>
 #include<vector>
-#include<memory>
 #include<cassert>
 
 class Solution {
     struct TreeNode {
-        TreeNode(int id) : id(id) {}
-        int id = 0;
         int subtree_size = 0;
-        std::vector<TreeNode*> neighbours;
+        std::vector<int> neighbours;
     };
 
 public:
     std::vector<int> sumOfDistancesInTree(int n, std::vector<std::vector<int>>& edges) {
         tree_size_ = n;
-        nodes_ = std::vector<std::unique_ptr<TreeNode>>(n);
-        distances_ = std::vector<int>(n);
+        nodes_.resize(n);
+        distances_.resize(n);
         if (edges.empty()) {
             return distances_;
         }
         BuildTree(edges);
-        TraverseTree(nodes_[0].get(), nullptr);
-        FinalPass(nodes_[0].get(), nullptr);
+        TraverseTree(0, -1);
+        FinalPass(0, -1);
         return distances_;
     }
 
 private:
-    std::vector<std::unique_ptr<TreeNode>> nodes_;
+    std::vector<TreeNode> nodes_;
     std::vector<int> distances_;
     int tree_size_ = 0;
 
     void BuildTree(std::vector<std::vector<int>>& edges) {
         for (const auto& edge : edges) {
-            if (!nodes_[edge[0]]) {
-                nodes_[edge[0]] = std::unique_ptr<TreeNode>(new TreeNode(edge[0]));
-            }
-            if (!nodes_[edge[1]]) {
-                nodes_[edge[1]] = std::unique_ptr<TreeNode>(new TreeNode(edge[1]));
-            }
-            nodes_[edge[0]]->neighbours.push_back(nodes_[edge[1]].get());
-            nodes_[edge[1]]->neighbours.push_back(nodes_[edge[0]].get());
+            nodes_[edge[0]].neighbours.push_back(edge[1]);
+            nodes_[edge[1]].neighbours.push_back(edge[0]);
         }
     }
 
-    int TraverseTree(TreeNode* node, TreeNode* parent) {
+    int TraverseTree(int node, int parent) {
         int subtree_sum = 0;
-        node->subtree_size = parent ? node->neighbours.size() - 1 : node->neighbours.size();
-        for (auto& next_node : node->neighbours) {
+        nodes_[node].subtree_size = parent != -1 ? nodes_[node].neighbours.size() - 1 : nodes_[node].neighbours.size();
+        for (int next_node : nodes_[node].neighbours) {
             if (next_node == parent) {
                 continue;
             }
-            subtree_sum += TraverseTree(next_node, node) + next_node->subtree_size + 1;
-            node->subtree_size += next_node->subtree_size;
+            subtree_sum += TraverseTree(next_node, node) + nodes_[next_node].subtree_size + 1;
+            nodes_[node].subtree_size += nodes_[next_node].subtree_size;
         }
-        distances_[node->id] = subtree_sum;
+        distances_[node] = subtree_sum;
         return subtree_sum;
     }
 
-    void FinalPass(TreeNode* node, TreeNode* parent) {
-        if (parent) {
-            int uptree_sum = (distances_[parent->id] - 1) - (distances_[node->id] + node->subtree_size);
-            distances_[node->id] += uptree_sum + ((tree_size_ - 1) - node->subtree_size);
+    void FinalPass(int node, int parent) {
+        if (parent != -1) {
+            int uptree_sum = (distances_[parent] - 1) - (distances_[node] + nodes_[node].subtree_size);
+            distances_[node] += uptree_sum + ((tree_size_ - 1) - nodes_[node].subtree_size);
         }
-        for (auto& next_node : node->neighbours) {
+        for (int next_node : nodes_[node].neighbours) {
             if (next_node == parent) {
                 continue;
             }
